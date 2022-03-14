e00b05b7ef2ebb4aeee0a31558ab53726203578c
ChescaRobertson <francescamarierobertson@hotmail.co.uk>
2022-03-14 11:15:08 +0000

    Toast notification project completed

---
+++ toast_notifications/script.js
@@ -0,0 +1,28 @@
+const btn = document.getElementById('button');
+const toasts = document.getElementById('toasts');
+
+const messages = ['Hello', 'Goodbye', 'Howdy', 'Smell ya later'];
+const types = ['success', 'info', 'error'];
+
+button.addEventListener('click', () => createNotification());
+
+function createNotification(message = null, type = null) {
+  const notif = document.createElement('div');
+  notif.classList.add('toast');
+  notif.classList.add(type ? type : getRandomType());
+
+  notif.innerText = message ? message : getRandomMessage();
+  toasts.appendChild(notif);
+
+  setTimeout(() => {
+    notif.remove();
+  }, 3000);
+}
+
+function getRandomMessage() {
+  return messages[Math.floor(Math.random() * messages.length)];
+}
+
+function getRandomType() {
+  return types[Math.floor(Math.random() * types.length)];
+}
